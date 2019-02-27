1796f45b21bf487437be63e63d795479c5f1a6ca
zst <821988519@qq.com>
2019-02-27 22:18:19 +0800

    Create class.py

---
+++ pythonProj/class.py
@@ -0,0 +1,18 @@
+class Calculator:       #首字母要大写，冒号不能缺
+    name='Good Calculator'  #该行为class的属性
+    price=18
+    def add(self,x,y):
+        print(self.name)
+        result = x + y
+        print(result)
+    def minus(self,x,y):
+        result=x-y
+        print(result)
+    def times(self,x,y):
+        print(x*y)
+    def divide(self,x,y):
+        print(x/y)
+cal=Calculator()
+cal.name
+cal.add(2,7)
+cal.divide(14,3)
