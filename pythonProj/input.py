ed6434c21fdf24bd45e7df378123132319cde644
zst <821988519@qq.com>
2019-02-27 22:45:26 +0800

    Create input.py

---
+++ pythonProj/input.py
@@ -0,0 +1,11 @@
+score=int(input('Please input your score: \n'))
+if score>=90:
+   print('Congradulation, you get an A')
+elif score >=80:
+    print('You get a B')
+elif score >=70:
+    print('You get a C')
+elif score >=60:
+    print('You get a D')
+else:
+    print('Sorry, You are failed ')
