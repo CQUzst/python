3d35cadcf00b014dc8f7fa83fe87936078044630
zst <821988519@qq.com>
2019-06-08 18:02:58 +0800

    Update 线性回归example.py

--- tensorflow/线性回归example.py
+++ tensorflow/线性回归example.py
@@ -1,27 +1,60 @@
+from __future__ import print_function
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
-# 创造散点图
-x_data = np.random.rand(100).astype(np.float32)
-y_data = x_data*0.1 + 0.3
-#可视化
-# fig = plt.figure()
-# ax1 = fig.add_subplot(1, 1, 1)
-# ax1.scatter(x_data, y_data)
-# plt.show()
-#w和b初始化
-Weights = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-biases = tf.Variable(tf.zeros([1]))
 
-y = Weights*x_data + biases
-loss = tf.reduce_mean(tf.square(y-y_data))
-optimizer = tf.train.GradientDescentOptimizer(0.1)
-train = optimizer.minimize(loss)
-init = tf.global_variables_initializer()  # 替换成这样就好
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    # add one more layer and return the output of this layer
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
+
+# Make up some real data
+x_data = np.linspace(-1,1,300)[:, np.newaxis]
+noise = np.random.normal(0, 0.1, x_data.shape)
+y_data = np.square(x_data) - 1 + noise
+
+# define placeholder for inputs to network
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+# add hidden layer
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+# add output layer
+prediction = add_layer(l1, 10, 1, activation_function=None)
+
+# the error between prediction and real data
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction),
+                     reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+
+
+init = tf.global_variables_initializer()
 sess = tf.Session()
-sess.run(init)          # Very important
+sess.run(init)
 
-for step in range(2001):
-    sess.run(train)
-    if step % 20 == 0:
-        print(step, sess.run(Weights), sess.run(biases))
+# plot the real data
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+ax.scatter(x_data, y_data)
+plt.ion()#本次运行请注释，全局运行不要注释
+plt.show()
+
+for i in range(10000):
+    # training
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 20 == 0:
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
+        print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+        prediction_value = sess.run(prediction, feed_dict={xs: x_data})
+        # plot the prediction
+        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+        plt.pause(0.001)
+plt.pause(10)
