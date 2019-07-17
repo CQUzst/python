47a710496b626ec7a064ada55ca233292f780b4b
zst <821988519@qq.com>
2019-07-17 15:12:18 +0800

    Update LeastSquaresCurverFitting.py

--- HW/LeastSquaresCurverFitting.py
+++ HW/LeastSquaresCurverFitting.py
@@ -3,55 +3,47 @@ import matplotlib.pyplot as plt
 from pylab import mpl
 mpl.rcParams['font.sans-serif'] = ['SimHei']# plt显示汉字
 
-x = np.arange(-1, 1, 0.01) # x 是n*1的数组
-y = ((x * x - 1) ** 3 + 1) * (np.cos(x * 2) + 0.6 * np.sin(x * 1.3)) # y 是n*1的数组
-
-y1 = y + (np.random.rand(len(x)) - 0.5)
 # print("x1=", x)
 # 读入数据
 file=open(r'D:/Data/coord.txt')
-x2=file.readline()
-x2=x2[3:-2].split(",")
-y2=file.readline()
-y2 = y2[3:-2].split(",")
+x=file.readline()
+x=x[3:-2].split(",")
+y=file.readline()
+y = y[3:-2].split(",")
 
 # 数据格式转换成np.array
-points=list([x2,y2])
+points=list([x,y])
 for i in range(len(points[0])):
-    x2[i] = np.array(float(x2[i])-418360)
-    y2[i] = np.array(float(y2[i])-2561325)
-x2=np.array(x2)
-y2=np.array(y2)
-x=x2
-y=y2
+    x[i] = np.array(float(x[i])-418360)
+    y[i] = np.array(float(y[i])-2561325)
+x=np.array(x)
+y=np.array(y)
+
 ##################################
 ### 核心程序
 # 使用函数y=ax^3+bx^2+cx+d对离散点进行拟合，最高次方需要便于修改，所以不能全部列举，需要使用循环
 # A矩阵
+maxOrder=7
 m = []
-for i in range(7):  # 这里选的最高次为x^7的多项式
+for i in range(maxOrder):  # 这里选最高次
     a = x ** (i) # a 是n*1的数组，a中是x[i]的i次方
     m.append(a)
 
 A = np.array(m).T # A中一行为每一个x点的系数，一共n行
-
 b = y.reshape(y.shape[0], 1) # reshape成一列
+
 # 构造成Ax=b的形式
-
-##################################
-
 def projection(A, b):
     AA = A.T.dot(A)  # A乘以A转置
     w = np.linalg.inv(AA).dot(A.T).dot(b) # w即为所求多项式系数
     print(w)
     return A.dot(w)
 
-
 yw = projection(A, b)
 yw.shape = (yw.shape[0],)
-
-plt.plot(x, y, color='g', linestyle='-', marker='', label=u"理想曲线")
-#plt.plot(x, y1, color='m', linestyle='', marker='o', label=u"已知数据点")
-plt.plot(x, yw, color='r', linestyle='', marker='.', label=u"拟合曲线")
+plt.figure(figsize=(10,10))# 创建画布
+plt.scatter(x,y,color='k',marker='.',label=u"数据点") # 散点
+#plt.plot(x, y, color='r',marker='.',label=u"数据点") #散点连线
+plt.plot(x, yw, color='r', linestyle='-', marker='.', label=u"拟合曲线")
 plt.legend(loc='upper left')
 plt.show()
