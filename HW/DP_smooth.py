456310c459fe1809636fb572f2bd89bd56ed25b2
zst <821988519@qq.com>
2019-07-19 12:01:21 +0800

    Update DP_smooth.py

--- HW/DP_smooth.py
+++ HW/DP_smooth.py
@@ -15,7 +15,7 @@ for line in file.readlines():
 x = np.array(x)
 y = np.array(y)
 
-
+#
 # #读入李博数据
 # file =open('D:/data/BP319U.txt','r', encoding='utf-8')
 # x=[]
@@ -36,6 +36,8 @@ plt.axis('equal')
 # y∗t+1=αyt+(1−α)y∗t
 x_list = [x[0]]
 y_list = [y[0]]
+update_x=[x[0]]
+update_y=[y[0]]
 x_list_double=[x_list[0]]
 y_list_double=[y_list[0]]
 x_double_exponential=[x[0]]
@@ -46,6 +48,8 @@ alpha=0.15
 for i in range(1,len(x)):
     real_time_x=x[i] # 实时最新坐标点x
     real_time_y=y[i] # 实时最新坐标点y
+    update_x.append(real_time_x)
+    update_y.append(real_time_y)
     x_list.append(alpha * real_time_x + (1 - alpha) * x_list[i - 1]) # 一阶指数平滑x
     y_list.append(alpha * real_time_y + (1 - alpha) * y_list[i - 1]) # 一阶指数平滑y
     x_list_double.append(alpha * x_list[i] + (1 - alpha) * x_list_double[i - 1]) # 二次一阶指数平滑x
@@ -54,35 +58,20 @@ for i in range(1,len(x)):
     x_b_double = (alpha / (1 - alpha)) * (x_list[i] - x_list_double[i])  # 二次指数平滑的x的系数b
     y_a_double = 2 * y_list[i] - y_list_double[i]                       # 二次指数平滑y的系数a
     y_b_double = (alpha / (1 - alpha)) * (y_list[i] - y_list_double[i])  # 二次指数平滑y的系数b
-    x_double_exponential.append(x_a_double+x_b_double*1) #二阶指数平滑x
+    x_double_exponential.append(x_a_double+x_b_double * 1) #二阶指数平滑x
     y_double_exponential.append(y_a_double + y_b_double * 1) #二阶指数平滑y
-    plt.scatter(x[i], y[i], color='k', marker='.', label=u"数据点")  # 散点
+    plt.clf()
+
+    plt.scatter(update_x, update_y, color='k', marker='.', label=u"数据点")  # 散点
     plt.plot(x_list, y_list, color='r', linestyle='-', marker='.', label=u"一次指数平滑")
     plt.plot(x_list_double, y_list_double, color='b', linestyle='-', marker='.', label=u"二次指数平滑")
     plt.plot(x_double_exponential, y_double_exponential, color='g', linestyle='-', marker='.', label=u"二阶指数平滑")
-    plt.pause(0.001)
+    plt.legend(loc='upper left')
+    string="DP"
+    string+=str(i)
+    string+=".png"
+    # plt.savefig(string)
+    plt.pause(0.1)
 plt.show()
 print("end")
-# def compute_single(alpha, s):
-#     return exponential_smoothing(alpha, s)
-#
-# def compute_double(alpha, s):
-#     s_single = compute_single(alpha, s)
-#     s_double = compute_single(alpha, s_single)
-#
-#     a_double = [0 for i in range(len(s))]
-#     b_double = [0 for i in range(len(s))]
-#
-#     for i in range(len(s)):
-#         a_double[i] = 2 * s_single[i] - s_double[i]  # 计算二次指数平滑的a
-#         b_double[i] = (alpha / (1 - alpha)) * (s_single[i] - s_double[i])  # 计算二次指数平滑的b
-#     return a_double, b_double,s_double
-
-# plt.figure(figsize=(10, 10))  # 创建画布
-# plt.scatter(x, y, color='k', marker='.', label=u"数据点")  # 散点
-# print(len(x),len(c1))
-# plt.plot(z1, z2, color='r', linestyle='-', marker='.', label=u"二阶指数平滑")
-# plt.plot(c1, c2, color='b', linestyle='-', marker='.', label=u"二次指数滑动")
-# plt.legend(loc='upper left')
-# plt.show()
 
